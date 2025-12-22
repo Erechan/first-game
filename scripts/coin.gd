3e503c1dec630098c7bae722262dfeaf161dfa40
Erechan <263432729@QQ.COM>
2025-12-22 10:59:51 +0800

    第五次提交，此次提交结果已实现拾取硬币音效，并且实现方法写在coin脚本中，不同于视频的实现方法

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var pickup_sound: AudioStreamPlayer2D = $PickupSound
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var coin: Area2D = $"."
+@onready var timer: Timer = $Timer
+
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
+	pickup_sound.play()
+	coin.visible = false
+	set_deferred("monitoring",false)
+	timer.start()
+
+func _on_timer_timeout():
+	print("time is out")
 	queue_free()
